339f3ab4c013b91a50c899effff2641e6765b6d5
Luba Kvartalnova <lubakvartalnova@yandex.ru>
2022-07-21 15:00:54 +0300

    division into functions

--- main.py
+++ main.py
@@ -4,33 +4,56 @@ import random
 words = ['слон', 'жираф', 'дельфин', 'горилла', 'попугай']
 word = list(random.choice(words))
 db = []
-attemp = 0
+attempt = 0
 
-print('*' * len(word))
-while attemp < 10:
-    letter = input('\nвведите букву: ')
+def input_user_letter():
+    return input("\nвведите букву: ")
 
+def check_letter(letter):
     if letter in word:
-        db.append(letter)
-    
+        add_letter_db(letter)
     else:
-        attemp += 1
-        print(f'у тебя осталось {10 - attemp} попыток!')
-    
+        global attempt
+        attempt += 1
+        show_attempt_status()
+
+def add_letter_db(arg_letter):
+    db.append(arg_letter)
+
+def show_attempt_status():
+    print(f'у тебя осталось {10 - attempt} попыток!')
+
+def create_guessed_part():
     result = []
-
     for let in word:
-
         if let in db:
-            result.append(let)
-        
+            result.append(let)       
         else:
             result.append('*')
-    
-    print('результат: ' + ''.join(result))
-    if result == word:
-        print('молодец!')
+    return result
+
+def print_result_guessed_word(guessed_part):
+    print('результат: ' + ''.join(guessed_part))
+
+def print_success_word():
+    print('ты выйграл, молодец!')
+
+def print_unsucces_word():
+    print('ты проиграл ;(')
+
+
+print('*' * len(word))
+while attempt < 10:
+
+    letter = input_user_letter()
+    check_letter(letter)
+        
+    guessed_part = create_guessed_part()
+    print_result_guessed_word(guessed_part)
+
+    if guessed_part == word:
+        print_success_word()
         break
         
 else:
-    print('ты проиграл!')
+    print_unsucces_word()
